4ec285179cb5385957e316fe939c69c3ddb8b5ce
mindsoldev <pharsan@yahoo.com>
2024-02-09 11:47:21 +0100

    Converted into a standalone application

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,68 +1,66 @@
 import { Component } from '@angular/core';
-import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx';
-import { Capacitor } from '@capacitor/core';
+import { IonicModule } from '@ionic/angular';
 import { Geolocation } from '@capacitor/geolocation';
 import { AndroidSettings, IOSSettings, NativeSettings } from 'capacitor-native-settings';
+import { Capacitor } from '@capacitor/core';
+import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
 })
 export class HomePage {
-
-  constructor() {}
-  // constructor(private locationAccuracy: LocationAccuracy) {}
+  constructor(private locationAccuracy: LocationAccuracy) {}
 
   async getCurrentLocation() {
     try {
-      console.log('Before checkPermissions');
       const permissionStatus = await Geolocation.checkPermissions();
-      console.log('Permission status 1: ', permissionStatus.location);
-      if (permissionStatus?.location != 'granted') {
-        console.log('Before requestPermissions');
+      console.log('Permission status: ', permissionStatus.location);
+      if(permissionStatus?.location != 'granted') {
         const requestStatus = await Geolocation.requestPermissions();
-        console.log('remission status 2: ', requestStatus.location);
-        if (requestStatus.location != 'granted') {
+        if(requestStatus.location != 'granted') {
           // go to location settings
           await this.openSettings(true);
-          return; 
+          return;
         }
       }
 
-      // if (Capacitor.getPlatform() == 'android') {
-      //   this.enableGps();
-      // }
+      if(Capacitor.getPlatform() == 'android') {
+        this.enableGps();
+      }
 
       let options: PositionOptions = {
         maximumAge: 3000,
         timeout: 10000,
         enableHighAccuracy: true
       };
-      const position = await Geolocation.getCurrentPosition();
+      const position = await Geolocation.getCurrentPosition(options);
       console.log(position);
-    } catch (e: any) {
+    } catch(e: any) {
       if(e?.message == 'Location services are not enabled') {
         await this.openSettings();
       }
       console.log(e);
-      throw(e);
     }
   }
 
   openSettings(app = false) {
-    console.log('open settings; aopp: ', app);
+    console.log('Now open settings...');
     return NativeSettings.open({
       optionAndroid: app ? AndroidSettings.ApplicationDetails : AndroidSettings.Location, 
-      optionIOS: app ? IOSSettings.App : IOSSettings.LocationServices 
+      optionIOS: app ? IOSSettings.App : IOSSettings.LocationServices
     });
   }
 
-  // async enableGps() {
-  //   const canRequest = await this.locationAccuracy.canRequest();
-  //   if(canRequest) {
-  //     await this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
-  //   }
-  // }
-  
+  async enableGps() {
+    const canRequest = await this.locationAccuracy.canRequest();
+    console.log('Enable GPS; can request: ', canRequest);
+    if(canRequest) {
+      await this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY);
+    }
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,22 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx';
-import { bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-//     LocationAccuracy,
-//     importProvidersFrom(IonicModule.forRoot({})),
-//     // provideRouter(routes),
-//   ],
-// });
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    LocationAccuracy,
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideRouter(routes),
+  ],
+});
